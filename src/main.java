7dabacd54cf0fb8c5c1ec6e636a8b58837dff133
RealFilllykung <filllykunggm@gmail.com>
2019-03-10 12:56:41 +0700

    Created SimpleWeightedGraph - Create method that can show the CityList - Added: put file into the SWG (SimpleWeightedGraph)

--- src/main.java
+++ src/main.java
@@ -19,15 +19,63 @@ class city {
 
 class MyDirectWeightGraph
 {
-    private HashMap <String, city> AllCity;
-    private HashSet <String> CityList;
+    private HashSet <String> CityList = new HashSet<String>();
     //We use HashSet to keep city list is because there are some of the duplicate city inside the file
 
     //Use SimpleWeightedGraph (for tbe sake of using direct property and collecting the weight of each nodes - nodes)
-    private SimpleWeightedGraph<String, DefaultWeightedEdge> SWG;
-    private Graph<String, DefaultWeightedEdge> G;
+    private SimpleWeightedGraph<String, DefaultWeightedEdge> SWG = new SimpleWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+    private Graph<String, DefaultWeightedEdge> G = (Graph<String, DefaultWeightedEdge>)SWG;
     private KruskalMinimumSpanningTree<String, DefaultWeightedEdge> KMS;
 
+    private void PrintCityList() {
+        ArrayList PrintCityList = new ArrayList(CityList);
+        for(int i = 0; i < PrintCityList.size(); i++)
+        {
+            System.out.println(PrintCityList.get(i));
+        }
+    }
+
+    MyDirectWeightGraph()
+    {
+        boolean askFile = true;
+        Scanner scanFile = null;
+
+        while(askFile)
+        {
+            try
+            {
+                System.out.print("Enter graph file: ");
+                Scanner scan = new Scanner(System.in);
+                String fileName = scan.next();
+
+                scanFile = new Scanner(new File(fileName));
+
+                askFile = false;
+            }
+            catch (Exception e)
+            {
+                System.err.println(e);
+            }
+        }
+
+        while(scanFile.hasNext())
+        {
+            String line = scanFile.nextLine();
+            String buf[] = line.split(" ");
+
+            String city1 = buf[0];
+            String city2 = buf[1];
+            int time = Integer.parseInt(buf[2]);
+
+            CityList.add(city1);
+            CityList.add(city2);
+
+            Graphs.addEdgeWithVertices(G,city1,city2,time);
+        }
+
+        //PrintCityList();  //Just for check that is there any duplicate city inside the list
+
+    }
 
 
 }
@@ -35,6 +83,6 @@ class MyDirectWeightGraph
 public class main {
     public static void main(String[] argv)
     {
-
+        new MyDirectWeightGraph();
     }
 }
